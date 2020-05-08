e7d178e33358206975d21642e37d95d905c38124
Danny Befumo <DPBefumo@gmail.com>
2020-05-08 14:34:12 -0400

    GET request trying to be added, but can't stay on page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const noteForm = document.querySelector('#note-form')
+const noteList = document.querySelector('#notes')
 
 noteForm.addEventListener('submit', function(event){
     event.preventDefault()
@@ -15,14 +16,35 @@ function createNewNote(noteText) {
         body: JSON.stringify ({item: noteText, created: moment().format()})
     })
     .then(response => response.json())
+    .then(() => renderNotes())
 }
 //3. render the list using the data that is now on the server
-//function doSomething(){
-//fetch('link', {
-    //method: 'GET'
-// })
-// .then(response => response.json())
-// .then(function (data) {
+function renderNotes() {
+    noteList.innerHTML = ''
+    fetch('http://localhost:3000/notes', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(function (notes) {
+            const list = document.createElement('ul')
+            list.id = 'note-list'
+            for (let note of notes) {
+                let listItem = document.createElement('li')
+                listItem.dataset.id = note.id
+                listItem.innerText = note.item
+                let editIcon = document.createElement('span')
+                editIcon.id = 'edit'
+                editIcon.classList.add ('fas', 'fa-pen-square')
+                listItem.appendChild(editIcon)
+                let deleteIcon = document.createElement('span')
+                deleteIcon.id = 'delete'
+                deleteIcon.classList.add('fas', 'fa-trash')
+                listItem.appendChild(deleteIcon)
+                list.appendChild(listItem)
+            }
+        noteList.appendChild(list)
+    })
+}
     //add the content to the DOM
     //how do i want to add it?
     //let item = document.createElement('whatever')
@@ -34,7 +56,5 @@ function createNewNote(noteText) {
     // }
     //identify a variable to querySelector to where i want this stuff to go
     //appendchild to that variable 
-// })
-//}
 
 //doSomething()
